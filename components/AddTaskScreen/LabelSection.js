e60366f1694fa8cf421c8eed70d32e0380766687
domas <domas.guiga@gmail.com>
2023-04-28 17:04:05 +0300

    labels can now be added or created when creating a new task

--- components/AddTaskScreen/LabelSection.js
+++ components/AddTaskScreen/LabelSection.js
@@ -13,6 +13,7 @@ export default function LabelSection(props) {
         },
         chosenLabelsContainer: {
             flexDirection: 'row',
+            flexWrap: 'wrap',
         },
         chosenLabel: {
             backgroundColor: '#AED3C5',
@@ -73,43 +74,144 @@ export default function LabelSection(props) {
         tickIcon: {
             height: 14,
             width: 14,
+        },
+        noLabels: {
+            // backgroundColor: '#AED3C5',
+            margin: 5,
+            paddingTop: 5,
+            paddingBottom: 5,
+            paddingLeft: 5,
+            paddingRight: 15,
+            color: '#666',
+            // fontWeight: 500,
+        },
+        createLabelOption: {
+            marginLeft: 10,
+            // backgroundColor: 'red',
+            paddingTop: 8,
+            paddingBottom: 8,
+            flexDirection: 'row',
+        },
+        addIcon: {
+            height: 18,
+            width: 18,
+            marginRight: 10,
         }
     });
 
     let initialLabels = [
         { labelName: 'univeras', checked: false, id: 0 },
-        { labelName: 'karo studijos', checked: true, id: 1 }
+        { labelName: 'karo studijos', checked: false, id: 1 }
     ];
 
-    const [chosenLabels, setChosenLabels] = useState([]);
+    let initialChosenLabels = initialLabels.filter((label) => label.checked);
+    // console.log(initialChosenLabels);
+
+    const [chosenLabels, setChosenLabels] = useState(initialChosenLabels);
     const [labels, setLabels] = useState(initialLabels);
+    const [filteredLabels, setFilteredLabels] = useState(initialLabels);
+    const [searchInput, setSearchInput] = useState("");
 
     const onLabelCheck = (option) => {
+
+        let isChecked = !option.checked;
+        
         setLabels((prevLabels) => {
-            newLabel = [...prevLabels];
-            newLabel[option.id].checked = !prevLabels[option.id].checked;
+            newLabel = prevLabels;
+            labelIndex = prevLabels.findIndex((label) => label.id == option.id);
+            newLabel[labelIndex].checked = !prevLabels[labelIndex].checked;
             return newLabel;
         });
+
+        if(isChecked) {
+            setChosenLabels((prevLabels) => {
+                return [...prevLabels, option];
+            });
+        } else {
+            let labelToRemove = chosenLabels.findIndex((label) => label.id == option.id);
+            // console.log("Labels", chosenLabels);
+            // console.log("Label to remove", labelToRemove);
+            setChosenLabels((prevChosenLabels) => {
+                let newLabelList = [...prevChosenLabels];
+                newLabelList.splice(labelToRemove, 1);
+                // console.log("NewLabelList after splicing:", newLabelList);
+                return newLabelList;
+            });
+        }
     }
 
-    useEffect(() => {
+    
+    const handleSearch = (input) => {
+        // console.log("Input:", input);
+        setSearchInput(input);
+        filterResults(input);
+    };
 
-    }, [labels]);
+    // console.log(searchInput);
+
+    const filterResults = (input) => {
+        results = labels.filter((label) => {
+            label.labelName = label.labelName.toLowerCase();
+            return label.labelName.startsWith(input.toLowerCase());
+        })
+        setFilteredLabels(results);
+        // console.log("Results:", results);
+    }
+
+    const handleNewLabel = (name) => {
+        console.log("New label:", name);
+        let newLabelObject = {
+            id: labels.length,
+            checked: false,
+            labelName: name,
+        }
+        newLabelList = [...labels, newLabelObject];
+        setLabels((prevLabels) => {
+            return [...prevLabels, newLabelObject];
+        })
+        setSearchInput("");
+        onLabelCheck(newLabelObject);
+        setFilteredLabels(newLabelList);
+    }
 
     return (
         <View style={styles.container}>
             <View style={styles.chosenLabelsContainer}>
-                <Text style={styles.chosenLabel}>univeras</Text>
-                <Text style={styles.chosenLabel}>karo studijos</Text>
+                {   
+                    chosenLabels.length == 0 ?
+                        <Text style={styles.noLabels}>No labels selected</Text>
+                    :
+                    chosenLabels.map((label) => {
+                        return (
+                            <Text style={styles.chosenLabel} key={label.id}>{label.labelName}</Text>
+                        )
+                    })
+                }
+                {/* <Text style={styles.chosenLabel}>univeras</Text>
+                <Text style={styles.chosenLabel}>karo studijos</Text> */}
             </View>
             <View style={styles.allLabelContainer}>
                 <TouchableOpacity style={styles.searchContainer}>
                     <Image style={styles.searchIcon} source={require("../../assets/search.png")} resizeMode='contain' />
-                    <TextInput style={styles.searchInput} placeholder="Search or create..."/>
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder="Search or create..."
+                        value={searchInput}
+                        onChangeText={text => handleSearch(text)}
+                    />
                 </TouchableOpacity>
                 <View style={styles.optionsContainer}>
                     {
-                        labels.map((label) => {
+                        filteredLabels.length == 0 && searchInput != "" ?
+                            <TouchableOpacity
+                                style={styles.createLabelOption}
+                                onPress={() => handleNewLabel(searchInput)}
+                            >
+                                <Image style={styles.addIcon} source={require("../../assets/add_green.png")} resizeMode='contain' />
+                                <Text>Create "{searchInput}"</Text>
+                            </TouchableOpacity>
+                        :
+                        filteredLabels.map((label) => {
                             return (
                                 <TouchableOpacity style={styles.labelOption} onPress={() => onLabelCheck(label)} key={label.id}>
                                     {
